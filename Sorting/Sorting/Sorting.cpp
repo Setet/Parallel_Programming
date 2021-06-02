df970a527ab2a736c5b95c9b22a826f64cd30f3c
Setet <Franklin03res@gmail.com>
2021-06-02 16:54:54 +0300

    v1.01

--- Sorting/Sorting/Sorting.cpp
+++ Sorting/Sorting/Sorting.cpp
@@ -6,7 +6,23 @@
 #include <time.h>
 using namespace std;
  
-#define SIZE 100
+#define SIZE 1000
+
+bool Sravn(int* A)
+{
+	for (int i = 1; i < SIZE; i++)
+	{
+		if (A[i - 1] > A[i])
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+			break;
+		}
+	}
+}
 
 void PrintArray(int arr[], int size)
 {
@@ -86,35 +102,119 @@ void BubbleSort(int arr[], int size)
 	}
 }
 
-void QuickSortParallel(int arr[], int low, int high)
+void ParallelOddEvenSort(int arr[], int size, int thrs)
 {
-	if (low < high)
+	omp_set_num_threads(thrs);
+
+	for (int phase = 0; phase < size; phase++)
 	{
-		int pi = partition(arr, low, high);
-
-
-#pragma omp task first private(arr,low,pi)
+		if (phase % 2 == 0)
 		{
-			QuickSortParallel(arr, low, pi - 1);
+#pragma omp for
+			for (int i = 1; i < size; i += 2)
+				CompareExchange(arr[i - 1], arr[i]);
 		}
-
-		//#pragma omp task firstprivate(arr, high,pi)
+		else
 		{
-			QuickSortParallel(arr, pi + 1, high);
+#pragma omp for
+			for (int i = 1; i < size - 1; i += 2)
+				CompareExchange(arr[i], arr[i + 1]);
 		}
 	}
 }
 
-void ShellSortParallel(int array[], int length)
-{
-	int h;
-	int i = 0;
-	for (h = length / 2; h > 0; h = h / 2)
+void QuickSort(int arr[], int lo, int hi) {
+	if (lo < hi) {
+		int i = lo, j = hi, x = arr[lo];
+		while (i < j) {
+			while (i < j && arr[j] >= x)
+				--j;
+			if (i < j)
+				arr[i++] = arr[j];
+			while (i < j && arr[i] < x)
+				++i;
+			if (i < j)
+				arr[j--] = arr[i];
+		}
+		arr[i] = x;
+		QuickSort(arr, lo, i - 1);
+		QuickSort(arr, i + 1, hi);
+	}
+}
+
+void QuickSortParallel(int* arr, int n, int thrs) {
+	omp_set_num_threads(thrs);
+
+	int i = 0, j = n - 1;
+	int pivot = arr[n / 2];
+
+	do {
+		while (arr[i] < pivot) {
+			i++;
+		}
+		while (arr[j] > pivot) {
+			j--;
+		}
+		if (i <= j) {
+			swap(arr[i], arr[j]);
+			i++; j--;
+		}
+	} while (i <= j);
+
+#pragma omp parallel shared(arr)
 	{
-#pragma omp parallel for shared( array, length, h, i)  default(none)
-		for (i = 0; i < h; i++)
+		if (j > 0) {
+			QuickSortParallel(arr, (j + 1), thrs);
+		}
+	}
+#pragma omp parallel shared(arr)
+	{
+		if (n > i) {
+			QuickSortParallel(arr + i, (n - i), thrs);
+		}
+	}
+}
+
+void ShellSort(int* arr, int size)
+{
+	int d = size / 2;
+
+	while (d > 0)
+	{
+		for (int i = 0; i < size - d; i++)
 		{
-			InsertSort(array, i, length, h);
+			int j = i;
+			while (j >= 0 && arr[j] > arr[j + d])
+			{
+				swap(arr[j], arr[j + d]);
+				j--;
+			}
+		}
+		d = d / 2;
+	}
+}
+
+void ShellSortParallel(int* arr, int size, int thrs)
+{
+	omp_set_num_threads(thrs);
+#pragma omp parallel firstprivate(size)
+	{
+		int m = omp_get_thread_num();
+		int step = size / 2;
+		while (step > 0)
+		{
+#pragma omp parallel for
+			for (int i = 0; i < (size - step); i++)
+#pragma omp critical 
+			{
+				int j = i;
+				while (j >= 0 && arr[j] > arr[j + step])
+				{
+					Swap(arr[j], arr[j + step]);
+					j--;
+				}
+			}
+			step = step / 2;
 		}
 	}
 }
@@ -122,32 +222,24 @@ void ShellSortParallel(int array[], int length)
 int main()
 {
 	double t, t1, t2;
-    setlocale(LC_ALL, "");
+	setlocale(LC_ALL, "");
 	//cout << "У вас в системе"<< omp_get_num_procs() <<"потоков"<< endl << "\n\n";
-
+	
+	cout << "Сортировка пузырьком" << endl;
 	//блок с сортировкой пузырьком
 	int* a = new int[SIZE];
 	for (int i = 0; i < SIZE; i++)
 	{
 		a[i] = SIZE - i;
 	}
-	t1= omp_get_wtime();
+	t1 = omp_get_wtime();
 	BubbleSort(a, SIZE);
 	t2 = omp_get_wtime();
 	t = t2 - t1;
 	//PrintArray(a, SIZE);
 	cout << "Время последовательного алгоритма Пузырька = " << t << endl;
 
-	bool sorted = true;
-	for (int i = 1; i < SIZE; i++)
-	{
-		if (a[i - 1] > a[i])
-		{
-			sorted = false;
-			break;
-		}
-	}
-	if (sorted == true)
+	if (!Sravn(a))
 	{
 		cout << "Массив отсортирован правильно!" << endl;
 	}
@@ -156,54 +248,173 @@ int main()
 		cout << "Массив отсортирован неправильно!" << endl;
 	}
 
-
-	//блок с быстрой сортировкой
-	int* b = new int[SIZE];
+	//блок  сортировкой чет-нечет
 	for (int i = 0; i < SIZE; i++)
 	{
-		b[i] = SIZE - i;
+		a[i] = SIZE - i;
 	}
 	t1 = omp_get_wtime();
-	QuickSortParallel(b, 0, SIZE - 1);
+	ParallelOddEvenSort(a, SIZE, 2);
 	t2 = omp_get_wtime();
 	t = t2 - t1;
 	//PrintArray(a, SIZE);
-	cout << "Время параллельного алгоритма Быстрой сортировки = " << t << endl;
+	cout << "Время параллельного чет-нечетный метода(2 потока) = " << t << endl;
 
-	if (!Сomparison(a, b))
+	if (!Sravn(a))
 	{
-		cout << "Массив отсортирован неправильно!" << endl;
+		cout << "Массив отсортирован правильно!" << endl;
 	}
 	else
 	{
+		cout << "Массив отсортирован неправильно!" << endl;
+	}
+
+	for (int i = 0; i < SIZE; i++)
+	{
+		a[i] = SIZE - i;
+	}
+	t1 = omp_get_wtime();
+	ParallelOddEvenSort(a, SIZE, 4);
+	t2 = omp_get_wtime();
+	t = t2 - t1;
+	//PrintArray(a, SIZE);
+	cout << "Время параллельного чет-нечетный метода(4 потока) = " << t << endl;
+
+	if (!Sravn(a))
+	{
+		cout << "Массив отсортирован правильно!" << "\n\n";
+	}
+	else
+	{
+		cout << "Массив отсортирован неправильно!" << "\n\n";
+	}
+
+	cout << "Быстрая сортировка" << endl;
+	//блок с быстрой сортировкой
+	for (int i = 0; i < SIZE; i++)
+	{
+		a[i] = SIZE - i;
+	}
+	t1 = omp_get_wtime();
+	QuickSort(a, 0, SIZE-1);
+	t2 = omp_get_wtime();
+	t = t2 - t1;
+	//PrintArray(a, SIZE);
+	cout << "Время последовательного алгоритма Быстрой сортировки = " << t << endl;
+
+	if (!Sravn(a))
+	{
 		cout << "Массив отсортирован правильно!" << endl;
+	}
+	else
+	{
+		cout << "Массив отсортирован неправильно!" << endl;
+	}
+
+	for (int i = 0; i < SIZE; i++)
+	{
+		a[i] = SIZE - i;
+	}
+	t1 = omp_get_wtime();
+	QuickSortParallel(a, SIZE, 2);
+	t2 = omp_get_wtime();
+	t = t2 - t1;
+	//PrintArray(a, SIZE);
+	cout << "Время параллельного алгоритма Быстрой сортировки(2 потока) = " << t << endl;
+
+	if (!Sravn(a))
+	{
+		cout << "Массив отсортирован правильно!" << endl;
+	}
+	else
+	{
+		cout << "Массив отсортирован неправильно!" << endl;
+	}
+
+	for (int i = 0; i < SIZE; i++)
+	{
+		a[i] = SIZE - i;
+	}
+	t1 = omp_get_wtime();
+	QuickSortParallel(a, SIZE, 4);
+	t2 = omp_get_wtime();
+	t = t2 - t1;
+	//PrintArray(a, SIZE);
+	cout << "Время параллельного алгоритма Быстрой сортировки(4 потока) = " << t << endl;
+
+	if (!Sravn(a))
+	{
+		cout << "Массив отсортирован правильно!" << "\n\n";
+	}
+	else
+	{
+		cout << "Массив отсортирован неправильно!" << "\n\n";
+	}
+
+	cout << "Сортировка Шелла" << endl;
+	//блок с сортировкой Шеллла
+	for (int i = 0; i < SIZE; i++)
+	{
+		a[i] = SIZE - i;
+	}
+	t1 = omp_get_wtime();
+	ShellSort(a, SIZE);
+	t2 = omp_get_wtime();
+	t = t2 - t1;
+	//PrintArray(a, SIZE);
+	cout << "Время последовательного алгоритма сортировки Шелла = " << t << endl;
+
+	if (!Sravn(a))
+	{
+		cout << "Массив отсортирован правильно!" << endl;
+	}
+	else
+	{
+		cout << "Массив отсортирован неправильно!" << endl;
 	}
 
 	//блок с сортировкой Шеллла
-	int* c = new int[SIZE];
 	for (int i = 0; i < SIZE; i++)
 	{
-		c[i] = SIZE - i;
+		a[i] = SIZE - i;
 	}
 	t1 = omp_get_wtime();
-	ShellSortParallel(c, SIZE);
+	ShellSortParallel(a, SIZE,2);
 	t2 = omp_get_wtime();
 	t = t2 - t1;
 	//PrintArray(a, SIZE);
-	cout << "Время параллельного алгоритма сортировки Шелла = " << t << endl;
+	cout << "Время параллельного алгоритма сортировки Шелла(2 потока) = " << t << endl;
 
-	if (!Сomparison(b, c))
+	if (!Sravn(a))
 	{
-		cout << "Массив отсортирован неправильно!\n" << endl;
+		cout << "Массив отсортирован правильно!" << endl;
 	}
 	else
 	{
-		cout << "Массив отсортирован правильно!\n" << endl;
+		cout << "Массив отсортирован неправильно!" << endl;
+	}
+
+	for (int i = 0; i < SIZE; i++)
+	{
+		a[i] = SIZE - i;
+	}
+	t1 = omp_get_wtime();
+	ShellSortParallel(a, SIZE, 4);
+	t2 = omp_get_wtime();
+	t = t2 - t1;
+	//PrintArray(a, SIZE);
+	cout << "Время параллельного алгоритма сортировки Шелла(4 потока) = " << t << endl;
+
+	if (!Sravn(a))
+	{
+		cout << "Массив отсортирован правильно!" << "\n\n";
+	}
+	else
+	{
+		cout << "Массив отсортирован неправильно!" << "\n\n";
 	}
 
 	delete[] a;
-	delete[] b;
-	delete[] c;
 
 	int choser;
 
